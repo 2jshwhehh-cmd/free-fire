ef0e78df7bd35f7e612593260122beb3f98d2f25
2jshwhehh-cmd <2jshwhehh@gmail.com>
2026-06-30 23:46:09 +0500

    Update App.tsx

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,6 +1,6 @@
-import { Toaster } from "./components/ui/toaster.tsx";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "./components/ui/tooltip.tsx";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routers } from "./router";
@@ -9,6 +9,7 @@ const queryClient = new QueryClient();
 
 const App = () => {
   const router = createBrowserRouter(routers);
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
